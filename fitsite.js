daf7b399500bd408d2fc9017b1dada26794feac9
Kristof <76616655+Kristofnewfort@users.noreply.github.com>
2025-01-12 18:04:22 +0000

    Sunday update

--- fitsite.js
+++ fitsite.js
@@ -87,3 +87,14 @@ document.addEventListener("DOMContentLoaded", function() {
   animate();
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const navToggle = document.getElementById("navToggle");
+  const navbar = document.getElementById("navbar");
+
+  navToggle.addEventListener("click", function () {
+    navbar.classList.toggle("expanded");
+    navbar.classList.toggle("collapsed");
+  });
+});
+
+
